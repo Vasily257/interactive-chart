bb63360809f1c04bfb37878f17617dce93c2b12f
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-10-18 14:34:52 +0500

    fix(home page): use the file instead of the fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 import { InteractiveChart } from './components/InteractiveChart';
 import { Donator } from './types/donator';
 import styles from './page.module.css';
+import data from '../../public/data.json';
 
 /** Получить данные донатера */
 const getDonatorData = async (): Promise<Donator> => {
-  const response = await fetch('https://run.mocky.io/v3/80119f7c-a545-46f9-ac6f-5f4bd130f2d3');
+  // Чтобы использовать мок-апи, надо добавить data.json в https://designer.mocky.io/ и получить ссылку.
+  // Но ссылка быстро устаревает, поэтому для стабильности настроено прямое обращение к файлу.
 
-  if (!response.ok) {
-    throw new Error(`Network response was not ok`);
-  }
+  // const response = await fetch('https://run.mocky.io/v3/80119f7c-a545-46f9-ac6f-5f4bd130f2d3');
 
-  return response.json();
+  // if (!response.ok) {
+  //   throw new Error(`Network response was not ok`);
+  // }
+
+  // return response.json();
+
+  return data;
 };
 
 /** Рендер-функция домашней страницы */
