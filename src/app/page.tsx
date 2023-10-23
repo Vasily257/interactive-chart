8c4599d0332be2d6fb77dbf3080172b6e54b0376
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2023-10-23 09:11:52 +0500

    fix(Home page): return fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,16 @@
 import { InteractiveChart } from './components/InteractiveChart/InteractiveChart';
 import { Donator } from './types/donator';
 import styles from './page.module.css';
-import data from '../../public/data.json';
 
 /** Получить данные донатера */
 const getDonatorData = async (): Promise<Donator> => {
-  // Чтобы использовать мок-апи, надо добавить data.json в https://designer.mocky.io/ и получить ссылку.
-  // Но ссылка быстро устаревает, поэтому для стабильности настроено прямое обращение к файлу.
+  const response = await fetch('https://run.mocky.io/v3/80119f7c-a545-46f9-ac6f-5f4bd130f2d3');
 
-  // const response = await fetch('https://run.mocky.io/v3/80119f7c-a545-46f9-ac6f-5f4bd130f2d3');
+  if (!response.ok) {
+    throw new Error(`Network response was not ok`);
+  }
 
-  // if (!response.ok) {
-  //   throw new Error(`Network response was not ok`);
-  // }
-
-  // return response.json();
-
-  return data;
+  return response.json();
 };
 
 /** Рендер-функция домашней страницы */
